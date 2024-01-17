3d20b772f28b520d4b9deff36ffd6896f0eec921
Noluthando25 <nonhlanhlafumba@gmail.com>
2024-01-16 23:53:37 -0800

    Nolu

--- monty.h
+++ monty.h
@@ -29,5 +29,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void pint(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 
 #endif
